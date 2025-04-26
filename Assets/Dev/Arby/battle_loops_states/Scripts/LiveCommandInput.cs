60656c7176967fe4c8bec446613ae90f0f405c35
def <defdef@defs-MacBook-Pro.local>
2025-04-27 02:38:27 +0700

    add dash with time param

--- Assets/Dev/Arby/battle_loops_states/Scripts/LiveCommandInput.cs
+++ Assets/Dev/Arby/battle_loops_states/Scripts/LiveCommandInput.cs
@@ -20,7 +20,13 @@ public class LiveCommandInput : MonoBehaviour
         Debug.Log($"Example of executing {SumoRobotCommand.Id}");
         var script = (InputProvider)SumoRobotCommand.inputProvider;
         script.EnqueueCommand(new AccelerateTimeAction(2f));
-        script.EnqueueCommand(new TurnAngleAction(4f));
+        script.EnqueueCommand(new TurnAngleAction(180f));
+        script.EnqueueCommand(new TurnAngleAction(-90f));
+
+        script.EnqueueCommand(new TurnLeftAngleAction(180f));
+        script.EnqueueCommand(new TurnRightAngleAction(90f));
+
+        script.EnqueueCommand(new DashTimeAction(2f));
         script.EnqueueCommand(new SkillAction(new StoneSkill()));
         script.EnqueueCommand(new SkillAction(new BoostSkill()));
     }
