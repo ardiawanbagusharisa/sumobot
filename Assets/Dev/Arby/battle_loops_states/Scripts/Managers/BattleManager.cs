579f58918c3e62c71532fece56df84d1d32b94ae
def <defdef@defs-MacBook-Pro.local>
2025-04-27 17:00:23 +0700

    add example of getting gameplay infos

--- Assets/Dev/Arby/battle_loops_states/Scripts/Managers/BattleManager.cs
+++ Assets/Dev/Arby/battle_loops_states/Scripts/Managers/BattleManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using BattleLoop;
 using CoreSumoRobot;
 using UnityEngine;
 
@@ -21,15 +22,20 @@ namespace BattleLoop
 
         public BattleInputType RobotInputType = BattleInputType.Keyboard;
         public BattleState CurrentState { get; private set; } = BattleState.Preparing;
+        public float BattleTime = 60f;
+        public BattleInfo BattleInfo;
+
         public event Action<BattleState> OnPostStateChanged;
         public event Action<float> OnCountdownChanged;
+        public event Action<BattleInfo> OnBattleInfoChanged;
         public GameObject SumoPrefab;
         public List<Transform> StartPositions = new List<Transform>();
-
 
         private List<GameObject> players = new List<GameObject>();
         private float countdownTime = 3f;
         private event Action<int> onPlayerAdded;
+        private Coroutine battleTimerCoroutine;
+        private Dictionary<string, BattleInfo> battleInfos;
 
         private void Awake()
         {
@@ -49,10 +55,14 @@ namespace BattleLoop
 
         private void Start()
         {
+            battleInfos = new Dictionary<string, BattleInfo>();
+            BattleInfo = new BattleInfo();
+
             onPlayerAdded += OnPlayerAdded;
             Prepare();
         }
 
+        #region Core Battle Manager
         private void Prepare()
         {
             TransitionToState(BattleState.Preparing);
@@ -64,7 +74,26 @@ namespace BattleLoop
         {
             Debug.Log($"Player registered: {index}");
 
+            var battlePlayerInfo = new BattlePlayerInfo();
+            battlePlayerInfo.Sumo = players[index].GetComponent<SumoRobot>();
+            battlePlayerInfo.SumoRobotController = players[index].GetComponent<SumoRobotController>();
+            battlePlayerInfo.Id = index;
+            battlePlayerInfo.Score = 0;
+
+            if (index == 0)
+            {
+
+                BattleInfo.LeftPlayer = battlePlayerInfo;
+            }
+            else
+            {
+                BattleInfo.RightPlayer = battlePlayerInfo;
+            }
+
+            ChangeBattleInfo(BattleInfo);
+
             players[index].GetComponent<SumoRobotController>().SetMovementEnabled(false);
+
             // Auto-start when 2 players registered
             if (players.Count == 2 && CurrentState == BattleState.Preparing)
             {
@@ -72,14 +101,6 @@ namespace BattleLoop
             }
         }
 
-        private void Update()
-        {
-            switch (CurrentState)
-            {
-                case BattleState.Battle:
-                    break;
-            }
-        }
 
         private void InitializePlayerByPosition(Transform playerPosition)
         {
@@ -88,7 +109,7 @@ namespace BattleLoop
             if (!players.Contains(player))
             {
                 var playerIdx = players.Count;
-                var isLeftSide = playerIdx % 2 == 0;
+                var isLeftSide = playerIdx == 0;
 
                 // [Todo]: Wrap with a function
                 player.GetComponent<SumoRobot>().IdInt = playerIdx;
@@ -105,6 +126,9 @@ namespace BattleLoop
 
         private IEnumerator StartCountdown()
         {
+            BattleInfo.Time = Mathf.CeilToInt(BattleTime);
+            ChangeBattleInfo(BattleInfo);
+
             TransitionToState(BattleState.Countdown);
             Debug.Log("Battle starting in...");
 
@@ -120,6 +144,31 @@ namespace BattleLoop
             StartBattle();
         }
 
+        private IEnumerator StartBattleTimer()
+        {
+            float timer = BattleTime;
+            while (timer > 0)
+            {
+                var time = Mathf.CeilToInt(timer);
+                Debug.Log(time);
+                BattleInfo.Time = time;
+
+                if (time <= 1)
+                {
+                    ChangeBattleInfo(BattleInfo);
+                    EndBattle(null);
+                    yield return new WaitForSeconds(1f);
+                }
+                else
+                {
+                    ChangeBattleInfo(BattleInfo);
+                }
+
+                yield return new WaitForSeconds(1f);
+                timer -= 1f;
+            }
+        }
+
         private void StartBattle()
         {
             Debug.Log("Battle Start!");
@@ -128,7 +177,30 @@ namespace BattleLoop
 
         private void EndBattle(GameObject winner)
         {
-            Debug.Log($"Winner: {winner.GetComponent<SumoRobot>().IdInt}");
+            BattleInfo.Rounds += 1;
+
+            // Draw
+            if (winner == null)
+            {
+                ChangeBattleInfo(BattleInfo);
+                TransitionToState(BattleState.BattleEnded);
+                StartCoroutine(ResetBattle());
+                return;
+            }
+
+            var winnerId = winner.GetComponent<SumoRobot>().IdInt;
+            Debug.Log($"Winner: {winnerId}");
+
+            if (winnerId == 0)
+            {
+                BattleInfo.LeftPlayer.Score += 1;
+            }
+            else
+            {
+                BattleInfo.RightPlayer.Score += 1;
+            }
+            ChangeBattleInfo(BattleInfo);
+
             TransitionToState(BattleState.BattleEnded);
             StartCoroutine(ResetBattle());
         }
@@ -147,6 +219,7 @@ namespace BattleLoop
 
         private void OnPlayerOutOfArena(int playerIdx)
         {
+            // Find player whos winner
             EndBattle(players.Find(p => p.GetComponent<SumoRobot>().IdInt != playerIdx));
         }
 
@@ -155,12 +228,17 @@ namespace BattleLoop
             Debug.Log($"State Transition: {CurrentState} → {newState}");
             CurrentState = newState;
 
+            BattleInfo.battleState = newState;
+            ChangeBattleInfo(BattleInfo);
+
             switch (newState)
             {
                 case BattleState.Battle:
+                    battleTimerCoroutine = StartCoroutine(StartBattleTimer());
                     players.ForEach((p) => p.GetComponent<SumoRobotController>().SetMovementEnabled(true));
                     break;
                 case BattleState.BattleEnded:
+                    StopCoroutine(battleTimerCoroutine);
                     players.ForEach((p) => p.GetComponent<SumoRobotController>().SetMovementEnabled(false));
                     break;
             }
@@ -168,7 +246,71 @@ namespace BattleLoop
 
             OnPostStateChanged?.Invoke(newState);
         }
+
+
+        private void ChangeBattleInfo(BattleInfo info)
+        {
+            BattleInfo = info;
+
+            OnBattleInfoChanged?.Invoke(info);
+        }
+        #endregion
+
+        #region Battle Info Getters
+        public Dictionary<string, BattleInfo> GetLog()
+        {
+            return battleInfos;
+        }
+        #endregion
     }
 
 
+}
+
+public enum BattleWinner
+{
+    Left,
+    Right,
+    Draw,
+    Ongoing,
+}
+
+public class BattleInfo
+{
+    public int Id;
+    public int Time;
+    public int Rounds;
+    public BattleState battleState;
+    public BattlePlayerInfo LeftPlayer;
+    public BattlePlayerInfo RightPlayer;
+    public BattleWinner GetWinner()
+    {
+        if (battleState != BattleState.BattleEnded)
+        {
+            return BattleWinner.Ongoing;
+        }
+
+        var leftScore = LeftPlayer.Score;
+        var rightScore = RightPlayer.Score;
+        if (leftScore == rightScore)
+        {
+            return BattleWinner.Draw;
+        }
+        else if (rightScore > LeftPlayer.Score)
+        {
+            return BattleWinner.Right;
+        }
+        else
+        {
+            return BattleWinner.Left;
+        }
+    }
+}
+
+public class BattlePlayerInfo
+{
+    public int Id;
+    public SumoRobot Sumo;
+    public SumoRobotController SumoRobotController;
+    public int Score = 0;
 }
