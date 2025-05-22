85accbc1a250b644253c68c909f414f625d1876b
arbyazra123 <arbyuyeach@gmail.com>
2025-05-22 17:04:49 +0700

    add minimal battle logging

---
+++ Assets/Dev/Arby/battle_loops_states/Scripts/Helper/LogTools.cs
@@ -0,0 +1,30 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using System.IO;
+using Unity.VisualScripting;
+using CoreSumoRobot;
+
+public static class Utility
+{
+    [MenuItem("Tools/Open Log Folder")]
+    public static void OpenLogFolder()
+    {
+        string logFolder = Path.Combine(Application.persistentDataPath, "Logs");
+
+        if (!Directory.Exists(logFolder))
+            Directory.CreateDirectory(logFolder);
+
+#if UNITY_EDITOR
+        EditorUtility.RevealInFinder(logFolder);
+#else
+        Debug.Log("Logs folder: " + logFolder);
+#endif
+    }
+
+    public static LogActorType ToLogActorType(this PlayerSide side)
+    {
+        return side == PlayerSide.Left ? LogActorType.LeftPlayer : LogActorType.RightPlayer;
+    }
+}
