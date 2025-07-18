2f122af96be68f12d77f914804ea6fcdd2e3a0b8
arbyazra123 <arbyuyeach@gmail.com>
2025-07-18 16:37:10 +0700

    update new GA

--- Assets/Scripts/Bot/Example/EA/GA/AIBot_EA_GA.cs
+++ Assets/Scripts/Bot/Example/EA/GA/AIBot_EA_GA.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using SumoCore;
 using SumoInput;
 using SumoManager;
@@ -5,100 +6,53 @@ using UnityEngine;
 
 namespace SumoBot
 {
+
+    [System.Serializable]
+    public class AIBot_EA_GA_Config
+    {
+        public float ScriptInterval = 0.2f;
+        public int PopulationSize = 10;
+        public int GenerationCount = 20;
+        public float MutationRate = 0.1f;
+        public string Name = "Genetic Algorithm Example";
+        public SkillType DefaultSkillType = SkillType.Boost;
+    }
+
     public class AIBot_EA_GA : Bot
     {
-        public override string ID => Name;
-        public override float Interval => ActionInterval;
-        public override SkillType SkillType => SkillType.Boost;
+        public AIBot_EA_GA_Config config = new();
 
-        public string Name => "GA";
+        public override string ID => config.Name;
+        public override float Interval => config.ScriptInterval;
+        public override SkillType SkillType => config.DefaultSkillType;
 
-        public float EvaluationInterval = 2f;
-        public float ActionInterval = 0.5f;
+        public static List<ISumoAction> PossibleActions = new() {
+            new TurnAction(InputType.Script, ActionType.TurnLeft, 0.1f),
+            new TurnAction(InputType.Script, ActionType.TurnLeft, 0.3f),
 
-        [HideInInspector]
-        public EA_GA_Data brain;
-        [HideInInspector]
-        public EA_GA_Data memory;
+            new TurnAction(InputType.Script, ActionType.TurnRight, 0.1f),
+            new TurnAction(InputType.Script, ActionType.TurnRight, 0.3f),
 
-        private float fitness;
-        private float evaluationTimer;
+            new AccelerateAction(InputType.Script, 0.1f),
 
+            new DashAction(InputType.Script),
+            new SkillAction(InputType.Script),
+        };
+
+        private List<AIBot_GA_Genome> population = new();
+        private AIBot_GA_Genome brain;
         private SumoAPI api;
-        private BattleState currState;
-
-        void EvaluateFitness()
-        {
-            if (memory == null || fitness > CalculateFitness(memory))
-                memory = brain.Clone();
-            else
-                brain = EA_GA_Data.Crossover(brain, memory);
-            brain.Mutate(0.1f);
-
-            fitness = 0f;
-        }
-
-        float CalculateFitness(EA_GA_Data data)
-        {
-            return api.DistanceNormalized();
-        }
-
-        void RandomizeBrain(EA_GA_Data data)
-        {
-            data.weightAngle = Random.Range(-1f, 1f);
-            data.weightDistance = Random.Range(-1f, 1f);
-            data.threshold = Random.Range(-1f, 1f);
-
-            Debug.Log($"[AIBot_EA_Basic][RandomizeBrain] weightAngle: {brain.weightAngle}, weightDistance: {brain.weightAngle}");
-        }
-
-        public void Decide()
-        {
-            float value = brain.weightDistance * api.DistanceNormalized();
-
-            Debug.Log($"[AIBot_EA_Basic] value: {value}, weightAngle: {brain.weightAngle}, normalizedAngle: {api.Angle(normalized: true)}, weightDistance: {brain.weightAngle}, normalizedDistance: {api.DistanceNormalized()}");
-
-            float accelDuration = 0.3f;
-            float signedAngleToEnemy = api.Angle();
-
-            if (Mathf.Abs(signedAngleToEnemy) < 20f)
-            {
-                if (Mathf.Abs(value) > 0.6f)
-                    Enqueue(new SkillAction(InputType.Script));
-                else if (Mathf.Abs(value) > 0.3f)
-                    Enqueue(new DashAction(InputType.Script));
-                else
-                    Enqueue(new AccelerateAction(InputType.Script, accelDuration));
-            }
-            else
-            {
-
-                if (signedAngleToEnemy < 0)
-                    Enqueue(new TurnAction(InputType.Script, ActionType.TurnLeft, ActionInterval));
-                else
-                    Enqueue(new TurnAction(InputType.Script, ActionType.TurnRight, ActionInterval));
-            }
-
-            fitness += 1f; // Example: reward for taking an action
-        }
-        public override void OnBotInit(PlayerSide side, SumoAPI botAPI)
-        {
-            api = botAPI;
-            brain = new EA_GA_Data();
-            RandomizeBrain(brain);
-        }
 
         public override void OnBotUpdate()
         {
-            if (evaluationTimer >= EvaluationInterval)
-            {
-                EvaluateFitness();
-                evaluationTimer = 0f;
-            }
-
-            Decide();
-
+            brain = Run();
+            Enqueue(brain.GetBestActionIndex());
             base.OnBotUpdate();
+        }
+
+        public override void OnBotInit(PlayerSide side, SumoAPI botAPI)
+        {
+            api = botAPI;
         }
 
         public override void OnBotCollision(ActionParameter param)
@@ -108,7 +62,77 @@ namespace SumoBot
 
         public override void OnBattleStateChanged(BattleState state)
         {
-            currState = state;
+        }
+
+        public float Evaluate(AIBot_GA_Genome genome)
+        {
+            float fitness = 0f;
+
+            ISumoAction action = genome.GetBestActionIndex();
+
+            SimulateResultAPI simResult = api.Simulate(action);
+            Vector3 aiDir = simResult.Rotation;
+            Vector3 aiPos = simResult.Position;
+
+            if (Vector3.Distance(aiPos, api.BattleInfo.ArenaPosition) > api.BattleInfo.ArenaRadius)
+                return -999f;
+
+            if (action is DashAction)
+            {
+                if (api.CanExecute(action))
+                    fitness += 2;
+            }
+            if (action is SkillAction)
+            {
+                if (api.CanExecute(action))
+                    fitness += 2;
+            }
+
+            float angleScore = api.Angle(myPos: aiPos, myRot: aiDir, normalized: true);
+            float distScore = api.DistanceNormalized(myPos: aiPos);
+
+            fitness += angleScore + distScore;
+
+            genome.fitness = fitness;
+            return fitness;
+        }
+
+
+
+        public AIBot_GA_Genome Run()
+        {
+            for (int i = 0; i < config.PopulationSize; i++)
+            {
+                population.Add(new AIBot_GA_Genome());
+            }
+
+            for (int gen = 0; gen < config.GenerationCount; gen++)
+            {
+                foreach (var genome in population)
+                {
+                    Evaluate(genome);
+                }
+
+                population.Sort((a, b) => b.fitness.CompareTo(a.fitness));
+
+                List<AIBot_GA_Genome> nextGen = new();
+
+                for (int i = 0; i < config.PopulationSize / 2; i++)
+                {
+                    nextGen.Add(population[i].Clone());
+
+                    var child = AIBot_GA_Genome.Crossover(
+                        population[i],
+                        population[Random.Range(0, config.PopulationSize / 2)]);
+
+                    child.Mutate(config.MutationRate);
+                    nextGen.Add(child);
+                }
+
+                population = nextGen;
+            }
+
+            return population[0];
         }
     }
 }
---
+++ Assets/Scripts/Bot/Example/EA/GA/EA_GA_Genome.cs
@@ -0,0 +1,56 @@
+using SumoCore;
+using UnityEngine;
+
+namespace SumoBot
+{
+    [System.Serializable]
+    public class AIBot_GA_Genome
+    {
+        public float[] weights = new float[AIBot_EA_GA.PossibleActions.Count];
+        public float fitness;
+
+        public AIBot_GA_Genome Clone()
+        {
+            AIBot_GA_Genome clone = new()
+            {
+                weights = (float[])weights.Clone()
+            };
+            return clone;
+        }
+
+        public void Mutate(float rate)
+        {
+            for (int i = 0; i < weights.Length; i++)
+            {
+                weights[i] += Random.Range(-rate, rate);
+            }
+        }
+
+        public static AIBot_GA_Genome Crossover(AIBot_GA_Genome a, AIBot_GA_Genome b)
+        {
+            var child = new AIBot_GA_Genome();
+            for (int i = 0; i < child.weights.Length; i++)
+            {
+                child.weights[i] = Random.value < 0.5f ? a.weights[i] : b.weights[i];
+            }
+            return child;
+        }
+
+        public ISumoAction GetBestActionIndex()
+        {
+            int bestIndex = 0;
+            float bestValue = float.MinValue;
+
+            for (int i = 0; i < weights.Length; i++)
+            {
+                if (weights[i] > bestValue)
+                {
+                    bestValue = weights[i];
+                    bestIndex = i;
+                }
+            }
+            return AIBot_EA_GA.PossibleActions[bestIndex];
+        }
+
+    }
+}
