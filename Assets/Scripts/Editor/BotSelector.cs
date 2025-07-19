1c43c80eaa98eea1f943623f9b8d4de5687fb6e0
arbyazra123 <arbyuyeach@gmail.com>
2025-07-19 21:54:34 +0700

    clear the UX of inspector bot manager

--- Assets/Scripts/Editor/BotSelector.cs
+++ Assets/Scripts/Editor/BotSelector.cs
@@ -29,8 +29,18 @@ public class BotSelector : Editor
 
         if (!selector.IsEnable) return;
 
+
         EditorGUILayout.LabelField("Select Bots for Match", EditorStyles.boldLabel);
 
+        if (Application.isPlaying)
+        {
+            GUI.enabled = false;
+            
+            selector.Left = (Bot)EditorGUILayout.ObjectField("Left (active)", selector.Left, typeof(Bot), false);
+            selector.Right = (Bot)EditorGUILayout.ObjectField("Right (active)", selector.Right, typeof(Bot), false);
+
+            GUI.enabled = true;
+        }
         selector.leftBotIndex = EditorGUILayout.Popup("Left Bot", selector.leftBotIndex, botNames);
         selector.rightBotIndex = EditorGUILayout.Popup("Right Bot", selector.rightBotIndex, botNames);
 
