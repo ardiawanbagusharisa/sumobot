729d5dacc09e5b7b3f82f5541befa176138505b4
arbyazra123 <arbyuyeach@gmail.com>
2025-07-20 11:09:02 +0700

    add amount of actions per interval for GA

--- Assets/Scripts/Bot/Example/EA/GA/AIBot_EA_GA.cs
+++ Assets/Scripts/Bot/Example/EA/GA/AIBot_EA_GA.cs
@@ -10,10 +10,11 @@ namespace SumoBot
     [System.Serializable]
     public class AIBot_EA_GA_Config
     {
-        public float ScriptInterval = 0.2f;
-        public int PopulationSize = 10;
-        public int GenerationCount = 20;
-        public float MutationRate = 0.1f;
+        public float ScriptInterval = 0.1f;
+        public int PopulationSize = 40;
+        public int GenerationCount = 7;
+        public float MutationRate = 0.5f;
+        public int ActionsPerInterval = 2;
         public string Name = "Genetic Algorithm Example";
         public SkillType DefaultSkillType = SkillType.Boost;
     }
@@ -34,6 +35,8 @@ namespace SumoBot
             new TurnAction(InputType.Script, ActionType.TurnRight, 0.3f),
 
             new AccelerateAction(InputType.Script, 0.1f),
+            new AccelerateAction(InputType.Script, 0.2f),
+            new AccelerateAction(InputType.Script, 0.3f),
 
             new DashAction(InputType.Script),
             new SkillAction(InputType.Script),
@@ -46,7 +49,8 @@ namespace SumoBot
         public override void OnBotUpdate()
         {
             brain = Run();
-            Enqueue(brain.GetBestActionIndex());
+            List<ISumoAction> actions = brain.GetBestAction(config.ActionsPerInterval);
+            actions.ForEach(action => Enqueue(action));
             base.OnBotUpdate();
         }
 
@@ -68,31 +72,34 @@ namespace SumoBot
         {
             float fitness = 0f;
 
-            ISumoAction action = genome.GetBestActionIndex();
+            List<ISumoAction> actions = genome.GetBestAction(config.ActionsPerInterval);
 
-            SimulateResultAPI simResult = api.Simulate(action);
-            Vector3 aiDir = simResult.Rotation;
-            Vector3 aiPos = simResult.Position;
-
-            if (Vector3.Distance(aiPos, api.BattleInfo.ArenaPosition) > api.BattleInfo.ArenaRadius)
-                return -999f;
-
-            if (action is DashAction)
+            foreach (var action in actions)
             {
-                if (api.CanExecute(action))
-                    fitness += 2;
+                SimulateResultAPI simResult = api.Simulate(action);
+                Vector3 aiDir = simResult.Rotation;
+                Vector3 aiPos = simResult.Position;
+
+                if (api.DistanceFromArena() > api.BattleInfo.ArenaRadius)
+                    return -999f;
+
+                if (action is DashAction)
+                {
+                    if (api.CanExecute(action))
+                        fitness += 2;
+                }
+
+                if (action is SkillAction)
+                {
+                    if (api.CanExecute(action))
+                        fitness += 2;
+                }
+
+                float angleScore = api.Angle(oriPos: aiPos, oriRot: aiDir, normalized: true);
+                float distScore = api.DistanceNormalized(oriPos: aiPos);
+
+                fitness += angleScore + distScore;
             }
-            if (action is SkillAction)
-            {
-                if (api.CanExecute(action))
-                    fitness += 2;
-            }
-
-            float angleScore = api.Angle(oriPos: aiPos, oriRot: aiDir, normalized: true);
-            float distScore = api.DistanceNormalized(oriPos: aiPos);
-
-            fitness += angleScore + distScore;
-
             genome.fitness = fitness;
             return fitness;
         }
