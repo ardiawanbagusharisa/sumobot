b600770228471d9db5690da27f3fccd500c02ad5
arbyazra123 <arbyuyeach@gmail.com>
2025-09-25 09:39:40 +0700

    fix batched flag

--- Assets/Scripts/Helper/BattleSimulator.cs
+++ Assets/Scripts/Helper/BattleSimulator.cs
@@ -60,7 +60,7 @@ namespace SumoHelper
                         ConfigEnd = end;
                 }
 
-                if (ConfigStart > 0 && ConfigEnd > 0)
+                if (ConfigStart > -1 && ConfigEnd > -1)
                 {
                     Batched = true;
                 }
